f447ba798dac28f69ffb0d9173b4fe7d8f1a7f94
ilauzon <ilauzon@my.bcit.ca>
2023-10-31 19:33:01 -0700

    added skeleton for navbar

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -10,11 +10,12 @@ function loadSkeleton() {
             // User is signed in.
             // Do something for the user here.
             // console.log($('#navbarPlaceholder').load('./text/nav_after_login.html'));
-            console.log($('#navbarPlaceholder').load('./text/nav_before_login.html'));
+            console.log($('#headerPlaceholder').load('./text/nav_before_login.html'));
+            console.log($('#navbarPlaceholder').load('./text/nav_after_login.html'));
             console.log($('#footerPlaceholder').load('./text/footer.html'));
         } else {
             // No user is signed in.
-            console.log($('#navbarPlaceholder').load('./text/nav_before_login.html'));
+            console.log($('#headerPlaceholder').load('./text/nav_before_login.html'));
             console.log($('#footerPlaceholder').load('./text/footer.html'));
         }
     });
