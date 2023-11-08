c4f3c0b4d49c68eb1121bad2ad2aa7a6e2054421
ilauzon <ilauzon@my.bcit.ca>
2023-11-08 08:37:29 -0800

    navbar color switching is functional

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,36 +1,71 @@
 // This file controls the dynamic style of the navbar.
 
+// I don't know why this fixes the window load event not triggering...
+setTimeout(() => {
+    let evt = document.createEvent('Event');
+    evt.initEvent('load', false, false);
+    window.dispatchEvent(evt);
+}, 300);
+
 const currentUrl = window.location.href.split("/")[3];
 
-var buyIcon = document.getElementById("buy-icon-obj").contentDocument.getElementById("buy-icon");
+//declaration to fix scope issues
+let buyIcon;
+let sellIcon;
+let offersIcon;
+let accountIcon;
 
-var sellIcon = document.getElementById("sell-icon-obj").contentDocument.getElementById("sell-icon");
+let unselectedColor = "#444444";
+let selectedColor = "#297373";
 
-var offersIcon = document.getElementById("offers-icon-obj").contentDocument.getElementById("offers-icon");
+document.getElementById("buy-icon-obj").addEventListener("load", () => {
+    console.log("buy icon loaded!");
+    buyIcon = document.getElementById("buy-icon-obj").contentDocument.getElementById("buy-icon");
+});
 
-var accountIcon = document.getElementById("account-icon-obj").contentDocument.getElementById("account-icon");
+document.getElementById("sell-icon-obj").addEventListener("load", () => {
+    console.log("sell icon loaded!");
+    sellIcon = document.getElementById("sell-icon-obj").contentDocument.getElementById("sell-icon");
+});
 
-if (currentUrl == "buy.html") {
-    buyIcon.style.fill = "#297373";
-    sellIcon.style.fill = "black";
-    offersIcon.style.fill = "black";
-    accountIcon.style.fill = "black";
-} else if (currentUrl == "sell.html") {
-    buyIcon.style.fill = "black";
-    sellIcon.style.fill = "#297373";
-    offersIcon.style.fill = "black";
-    accountIcon.style.fill = "black";
-} else if (currentUrl == "mycars.html") {
-    buyIcon.style.fill = "black";
-    sellIcon.style.fill = "black";
-    offersIcon.style.fill = "#297373";
-    accountIcon.style.fill = "black";
-} else if (currentUrl == "account.html") {
-    buyIcon.style.fill = "black";
-    sellIcon.style.fill = "black";
-    offersIcon.style.fill = "black";
-    accountIcon.style.fill = "#297373";
+document.getElementById("offers-icon-obj").addEventListener("load", () => {
+    console.log("offers icon loaded!");
+    offersIcon = document.getElementById("offers-icon-obj").contentDocument.getElementById("offers-icon");
+});
+
+document.getElementById("account-icon-obj").addEventListener("load", () => {
+    console.log("account icon loaded!");
+    accountIcon = document.getElementById("account-icon-obj").contentDocument.getElementById("account-icon");
+});
+
+window.addEventListener("load", determineColor);
+
+function determineColor() {
+    console.log("hello from body!");
+    if (currentUrl == "buy.html") {
+        buyIcon.style.fill = selectedColor;
+        sellIcon.style.fill = unselectedColor;
+        offersIcon.style.fill = unselectedColor;
+        accountIcon.style.fill = unselectedColor;
+    } else if (currentUrl == "sell.html") {
+        buyIcon.style.fill = unselectedColor;
+        sellIcon.style.fill = selectedColor;
+        offersIcon.style.fill = unselectedColor;
+        accountIcon.style.fill = unselectedColor;
+    } else if (currentUrl == "mycars.html") {
+        buyIcon.style.fill = unselectedColor;
+        sellIcon.style.fill = unselectedColor;
+        offersIcon.style.fill = selectedColor;
+        accountIcon.style.fill = unselectedColor;
+    } else if (currentUrl == "account.html") {
+        buyIcon.style.fill = unselectedColor;
+        sellIcon.style.fill = unselectedColor;
+        offersIcon.style.fill = unselectedColor;
+        accountIcon.style.fill = selectedColor;
+    }
 }
+
+
 
 // function changeNavColor(id) {
 //     if (id == "buy-icon-container") {
