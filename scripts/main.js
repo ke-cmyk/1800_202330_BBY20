c162fc280737c8bc7109966527e2a5daffa054f3
aathasri <asriharan@my.bcit.ca>
2023-11-08 00:28:23 -0800

    made partially functional mycars page. issue with html loading but works when manually loaded in console

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
+const userID = firebase.auth().currentUser.uid;
+
 function writeMyPosts() {
     // Define a reference to the user document
-    const userID = firebase.auth().currentUser.uid;
+    // const userID = firebase.auth().currentUser.uid;
     var userDocRef = db.collection("users").doc(userID);
 
     // Define a reference to the "myPosts" collection within the user document
