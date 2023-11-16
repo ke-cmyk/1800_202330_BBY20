8992ddaf4fd936a25ff5c754b744cd028aab504f
ilauzon <ilauzon@my.bcit.ca>
2023-11-16 08:33:35 -0800

    linked vehicle data per request to request preview html

--- scripts/buy.js
+++ scripts/buy.js
@@ -20,26 +20,30 @@ function displayCardsDynamically() {
         userDoc.data().requests.forEach(requestId => {
             if (requestId != "") {
                 db.collection("requests").doc(requestId).get()
-                .then(doc => {
-                    var make = doc.data().make;
-                    var model = doc.data().model;
-                    var year = doc.data().year;
-        
-                    let newcard = cardTemplate.content.cloneNode(true);
-        
-                    //update title and text and image
-                    newcard.querySelector('.request-car-name').innerHTML = year + " " + make + " " + model;
-                    // newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
-        
-                    //Optional: give unique ids to all elements for future use
-                    // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
-                    // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
-                    // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
-        
-                    //attach to gallery, Example: "hikes-go-here"
-                    document.getElementById("requests-container").appendChild(newcard);
-        
-                    //i++;   //Optional: iterate variable to serve as unique ID
+                .then(requestDoc => {
+                    db.collection("vehicles").doc(requestDoc.data().vehicleID).get()
+                    .then(vehicleDoc => {
+                        var make = vehicleDoc.data().make;
+                        var model = vehicleDoc.data().model;
+                        var year = vehicleDoc.data().year;
+            
+                        let newcard = cardTemplate.content.cloneNode(true);
+            
+                        //update title and text and image
+                        newcard.querySelector('.request-car-name').innerHTML = year + " " + make + " " + model;
+                        // newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
+            
+                        //Optional: give unique ids to all elements for future use
+                        // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+                        // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+                        // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+            
+                        //attach to gallery, Example: "hikes-go-here"
+                        document.getElementById("requests-container").appendChild(newcard);
+            
+                        //i++;   //Optional: iterate variable to serve as unique ID
+                    })
+
                 })
             }
 
